da28655a8ff23ac2ea3e1f7f30b546f34b87b6dc
natelincyber <natelin200510@gmail.com>
2022-07-01 13:05:20 -0700

    cleaned up code and added documentation

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,67 +1,59 @@
 var slideIndex = 0;
 var prevIndex;
 
+// adds CSS class that triggers entrance transition
+const fadeIn = (x, i) => {
+  x[i].classList.add("slide-in-left");
+};
 
-
-
-const fadeIn = (x,i) => {
-  x[i].classList.add('slide-in-left')
+// adds CSS class that triggers exit animation
+const fadeOut = (x, i) => {
+  x[i].classList.add("fade-out-right");
 };
 
 
-const fadeOut = (x,i) => {
-  x[i].classList.add('fade-out-right');
-  
-
-  console.log('done')
-}
-
-
-
+// main loop
 function carousel() {
   var i;
   var x = document.getElementsByClassName("slideItem");
- 
-  for (i = 0; i < x.length; i++) { // hides all images
+
+  for (i = 0; i < x.length; i++) {
+    // hides all images and removes added effects
     x[i].style.display = "none";
-
-    x[i].classList.remove('slide-in-left');
+    x[i].classList.remove("slide-in-left");
     x[i].style.zIndex = "0";
-  
   }
+
   slideIndex++;
-  
 
-  // if (slideIndex-2 == -2) {prevIndex = 3} else if (slideIndex-2 == -1) {prevIndex=0} else {prevIndex = slideIndex - 2}
+  //0 3  1 0  2 1  3 2
+  // 0    1    2    3
 
+  // prevents IndexOutOfRange exception
+  if (slideIndex > x.length - 1) {
+    slideIndex = 0;
+  }
 
-  //0 3  1 0  2 1  3 2 
-  // 0    1    2    3    
-
-  if (slideIndex > x.length-1) { slideIndex = 0 }
-  
-
+  // unhide the image that is about to be shown
   x[slideIndex].style.display = "block";
-  x[slideIndex].style.zIndex = "1"
+  x[slideIndex].style.zIndex = "1";
 
-
+  // logic ot prevent outOfRange exceptions
   if (slideIndex == 0) {
     prevIndex = 3;
   } else {
     prevIndex = slideIndex - 1;
   }
- 
 
+  // unhide previously shown image to add crossfade effect
   x[prevIndex].style.display = "block";
   x[prevIndex].style.zIndex = "0";
 
+  // trigger animations
+  fadeIn(x, slideIndex);
+  fadeOut(x, prevIndex);
 
-
-  fadeIn(x,slideIndex);
-  fadeOut(x,prevIndex);
   setTimeout(carousel, 2000); // Change image every 2 seconds
-  
 }
 
 carousel();
-
