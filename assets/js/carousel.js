b0a3923744398353b4e49b42045880bb4292e145
natelincyber <natelin200510@gmail.com>
2022-06-30 21:16:35 -0700

    Improved carousel transitions

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,44 +1,63 @@
 var slideIndex = 0;
 var numImgs = 4;
+var prevIndex;
 
 
-const newspaperSpinning = [
-  { transform: 'rotate(0) scale(1)' },
-  { transform: 'rotate(360deg) scale(0)' }
-];
-const newspaperTiming = {
-  duration: 2000,
-  iterations: 1,
-}
 
 
-const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-
-const fade = async (x,i) => {
-  j=100;
-  x[i].classList.add('fade-in-left')
-  // while(j>40) {
-  //   x[i].style.style.animation = 'fading 2s infinite'
-  //   await wait(60);
-  //   j-=1
-  // }
+const fadeIn = (x,i) => {
+  x[i].classList.add('slide-in-left')
 };
 
 
+const fadeOut = (x,i) => {
+  x[i].classList.add('fade-out-right');
+  
 
+  console.log('done')
+}
 
 
 
 function carousel() {
   var i;
   var x = document.getElementsByClassName("slideItem");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+ 
+  for (i = 0; i < x.length; i++) { // hides all images
+    x[i].style.opacity = "none";
+    x[i].classList.remove('slide-in-left');
+    x[i].style.zIndex = "0";
   }
   slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1}
-  x[slideIndex-1].style.display = "block";
-  fade(x,slideIndex-1)
+  
+
+  // if (slideIndex-2 == -2) {prevIndex = 3} else if (slideIndex-2 == -1) {prevIndex=0} else {prevIndex = slideIndex - 2}
+
+
+  //0 3  1 0  2 1  3 2 
+  // 0    1    2    3    
+
+  if (slideIndex > x.length-1) { slideIndex = 0 }
+  
+
+  x[slideIndex].style.display = "block";
+  x[slideIndex].style.zIndex = "1"
+
+
+  if (slideIndex == 0) {
+    prevIndex = 3;
+  } else {
+    prevIndex = slideIndex - 1;
+  }
+  console.log(slideIndex, prevIndex, slideIndex);
+  
+  console.log(x[prevIndex])
+  x[prevIndex].style.display = "block";
+  x[prevIndex].style.zIndex = "0";
+
+  fadeIn(x,slideIndex);
+
+  
   setTimeout(carousel, 2000); // Change image every 2 seconds
   
 }
